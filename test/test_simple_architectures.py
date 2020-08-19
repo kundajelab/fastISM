d7a99deaa7a10c641cbc2eaa095b691c0e356802
Surag Nair <suragnair@hotmail.com>
2020-08-18 18:35:39 -0700

    more tests, failing even conv with same padding

--- test/test_simple_architectures.py
+++ test/test_simple_architectures.py
@@ -38,7 +38,18 @@ class TestSimpleArchitectures(unittest.TestCase):
         fast_ism_model = fast_ism.FastISM(model, 100, 4, test_correctness=False)
         
         self.assertTrue(fast_ism_model.test_correctness())
-    
+
+    def test_conv_even_kernel_same_padding_fc(self):
+        inp = tf.keras.Input((100,4))
+        x = tf.keras.layers.Conv1D(20, 4, padding='same')(inp)
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(1)(x)
+        model = tf.keras.Model(inputs=inp, outputs=x)
+        
+        fast_ism_model = fast_ism.FastISM(model, 100, 4, test_correctness=False)
+        
+        self.assertTrue(fast_ism_model.test_correctness())
+        
     def test_conv_dilated_fc(self):
         inp = tf.keras.Input((100,4))
         x = tf.keras.layers.Conv1D(20, 3, dilation_rate=3)(inp)
@@ -80,13 +91,32 @@ class TestSimpleArchitectures(unittest.TestCase):
         inp = tf.keras.Input((100,4))
         x = tf.keras.layers.Conv1D(10, 7, padding='same')(inp)
         x = tf.keras.layers.MaxPooling1D(3)(x)
-        x = tf.keras.layers.Conv1D(10, 3)(inp)
+        x = tf.keras.layers.Conv1D(10, 3)(x)
         x = tf.keras.layers.MaxPooling1D(2)(x)
         x = tf.keras.layers.Flatten()(x)
         x = tf.keras.layers.Dense(2)(x)
         model = tf.keras.Model(inputs=inp, outputs=x)
         
         fast_ism_model = fast_ism.FastISM(model, 100, 4, test_correctness=False)
+        
+        self.assertTrue(fast_ism_model.test_correctness())
+    
+    def test_four_conv_maxpool_two_fc(self):
+        inp = tf.keras.Input((200,4))
+        x = tf.keras.layers.Conv1D(10, 7, padding='same')(inp)
+        x = tf.keras.layers.MaxPooling1D(2)(x)
+        x = tf.keras.layers.Conv1D(20, 4, padding='same')(inp)
+        x = tf.keras.layers.MaxPooling1D(2)(x)
+        x = tf.keras.layers.Conv1D(30, 2, padding='valid')(x)
+        x = tf.keras.layers.MaxPooling1D(2)(x)
+        x = tf.keras.layers.Conv1D(10, 6, padding='same')(x)
+        x = tf.keras.layers.MaxPooling1D(2)(x)
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(20)(x)
+        x = tf.keras.layers.Dense(1)(x)
+        model = tf.keras.Model(inputs=inp, outputs=x)
+        
+        fast_ism_model = fast_ism.FastISM(model, 200, 4, test_correctness=False)
         
         self.assertTrue(fast_ism_model.test_correctness())
         
