93cee6e7acf5a341ab20a0fb7590c94e2770bee2
Surag Nair <suragnair@hotmail.com>
2020-08-20 21:31:47 +0530

    moved bpnet arch to model file

---
+++ fastISM/models/bpnet.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+
+def bpnet_model(seqlen=1000, numchars=4, num_dilated_convs=5, num_tasks=1,
+                name='bpnet_model'):
+    # original as per https://www.biorxiv.org/content/10.1101/737981v1.full.pdf
+    inp = tf.keras.layers.Input(shape=(seqlen, 4))
+    x = tf.keras.layers.Conv1D(
+        64, kernel_size=25, padding='same', activation='relu')(inp)
+    
+    for i in range(num_dilated_convs):
+        conv_x = tf.keras.layers.Conv1D(
+            64, kernel_size=3, padding='same', activation='relu', dilation_rate=2**i)(x)
+        x = tf.keras.layers.Add()([conv_x, x])
+    bottleneck = x
+    
+    # heads
+    outputs = []
+    for _ in range(num_tasks):
+        # profile shape head
+        px = tf.keras.layers.Reshape((-1, 1, 64))(bottleneck)
+        px = tf.keras.layers.Conv2DTranspose(
+            2, kernel_size=(25, 1), padding='same')(px)
+        outputs.append(tf.keras.layers.Reshape((-1, 2))(px))
+
+        # total counts head
+        cx = tf.keras.layers.GlobalAvgPool1D()(bottleneck)
+        outputs.append(tf.keras.layers.Dense(2)(cx))
+
+    model = tf.keras.Model(inputs=inp, outputs=outputs)
+
+    return model
--- test/test_example_architectures.py
+++ test/test_example_architectures.py
@@ -1,9 +1,10 @@
-import tensorflow as tf
 import unittest
+import tensorflow as tf
 
 from context import fastISM
 from fastISM.models.basset import basset_model
 from fastISM.models.factorized_basset import factorized_basset_model
+from fastISM.models.bpnet import bpnet_model
 
 
 # Takes a few mins!
@@ -41,58 +42,16 @@ class TestExampleArchitectures(unittest.TestCase):
         self.assertTrue(fast_ism_model.test_correctness())
 
     def test_bpnet_5_dilated_500(self):
-        # body
-        inp = tf.keras.layers.Input(shape=(500, 4))
-        x = tf.keras.layers.Conv1D(
-            64, kernel_size=25, padding='same', activation='relu')(inp)
-
-        for i in range(1, 6):
-            conv_x = tf.keras.layers.Conv1D(
-                64, kernel_size=3, padding='same', activation='relu', dilation_rate=2**i)(x)
-            x = tf.keras.layers.Add()([conv_x, x])
-
-        bottleneck = x
-
-        # single task
-        # heads
-        # profile shape head
-        px = tf.keras.layers.Reshape((-1, 1, 64))(bottleneck)
-        px = tf.keras.layers.Conv2DTranspose(
-            2, kernel_size=(25, 1), padding='same')(px)
-        # total counts head
-        cx = tf.keras.layers.GlobalAvgPool1D()(bottleneck)
-
-        model = tf.keras.Model(inputs=inp, outputs=[px, cx])
-
+        model = bpnet_model(seqlen=500, num_dilated_convs=5)
+        
         fast_ism_model = fastISM.FastISM(
             model, test_correctness=False)
 
         self.assertTrue(fast_ism_model.test_correctness())
 
     def test_bpnet_9_dilated_1000(self):
-        # body
-        inp = tf.keras.layers.Input(shape=(1000, 4))
-        x = tf.keras.layers.Conv1D(
-            64, kernel_size=25, padding='same', activation='relu')(inp)
-
-        for i in range(1, 10):
-            conv_x = tf.keras.layers.Conv1D(
-                64, kernel_size=3, padding='same', activation='relu', dilation_rate=2**i)(x)
-            x = tf.keras.layers.Add()([conv_x, x])
-
-        bottleneck = x
-
-        # single task
-        # heads
-        # profile shape head
-        px = tf.keras.layers.Reshape((-1, 1, 64))(bottleneck)
-        px = tf.keras.layers.Conv2DTranspose(
-            2, kernel_size=(25, 1), padding='same')(px)
-        # total counts head
-        cx = tf.keras.layers.GlobalAvgPool1D()(bottleneck)
-
-        model = tf.keras.Model(inputs=inp, outputs=[px, cx])
-
+        model = bpnet_model(seqlen=1000, num_dilated_convs=9)
+        
         fast_ism_model = fastISM.FastISM(
             model, test_correctness=False)
 
